336f76d5aefdf2f4867fc06a0f20f9fca19d42d1
aribowobob <aribowo.susetyo@gmail.com>
2024-07-23 08:18:50 +0700

    feat(components): create number-input

---
+++ src/components/input/number-input.tsx
@@ -0,0 +1,66 @@
+import type { FC } from 'react';
+
+import { MinusCircleIcon, PlusCircleIcon } from '@heroicons/react/24/outline';
+import clsx from 'clsx';
+
+type NumberInputProps = {
+  value: number;
+  onChange: (value: number) => void;
+  min?: number;
+  max?: number;
+  step?: number;
+};
+
+const NumberInput: FC<NumberInputProps> = ({
+  value,
+  onChange,
+  min = 1,
+  max = 999999999,
+  step = 1,
+}) => {
+  const handleIncrement = () => {
+    let nextValue = value;
+
+    if (value + step <= max) {
+      nextValue += step;
+    }
+
+    onChange(nextValue);
+  };
+
+  const handleDecrement = () => {
+    let nextValue = value;
+
+    if (value - step >= min) {
+      nextValue -= step;
+    }
+
+    onChange(nextValue);
+  };
+
+  return (
+    <div className="p-1 border border-slate-500 gap-4 rounded-full flex items-center">
+      <button className="p-0" onClick={handleDecrement}>
+        <MinusCircleIcon
+          className={clsx(
+            'w-6 h-6',
+            value <= min ? 'text-slate-300' : 'text-red-600'
+          )}
+        />
+      </button>
+
+      <span className="min-w-[52px] text-center leading-6">{value}</span>
+
+      <button className="p-0" onClick={handleIncrement}>
+        <PlusCircleIcon
+          className={clsx(
+            'w-6 h-6',
+            value <= min ? 'text-slate-300' : 'text-teal-600'
+          )}
+        />
+      </button>
+    </div>
+  );
+};
+
+export default NumberInput;
