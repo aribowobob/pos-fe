19bc1164f6d32b5c72d96ed5a72e9a860359e849
aribowobob <aribowo.susetyo@gmail.com>
2025-07-13 13:26:47 +0700

    chore: remove unused files

--- app/(dashboard)/transactions/sale/hooks/__tests__/__mocks__/fetchers.ts
+++
@@ -1,6 +0,0 @@
-export const getSalesCartItemsFn = jest.fn();
-export const addToSalesCartFn = jest.fn();
-export const updateSalesCartItemFn = jest.fn();
-export const deleteSalesCartItemFn = jest.fn();
-export const clearSalesCartFn = jest.fn();
-export const createSalesOrderFn = jest.fn();
